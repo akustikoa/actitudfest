f81ae9c406abf13a8dc0e955f7d692e11e6b14b5
akustikoa <156797851+akustikoa@users.noreply.github.com>
2026-03-13 15:40:02 +0100

    aobut basic

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,339 @@
+const archiveData = `1	19/02/2002	Camping(Granollers) + No time to Think (Granollers)
+2	18/04/2003	Aghast(Fr) + Tekken(Fr)
+3	13/05/2003	Lo mueso(maresme) + Nisei (Bcn)
+4	28/06/2003	Dr. Explosion(Gijon) + No more lies(SFG)
+5	22/11/2003	Memorikol (Vidreres)
+6	26/11/2003	The Nederbietles(Holanda) + Overside(Girona)
+7	19/12/2003	Henry(Cadiz) + Airun(Banyoles)
+8	27/12/2003	Audience(Pais Basc) + Allegro Andante(Cassa de la Selva)
+9	14/02/2004	Death Knell of Gaia(Bcn) + Outbreak(Vidreres)
+10	26/02/2004	Imply in all(Fr) + Bollichaos
+11	12/03/2004	Tuesday Afernoon(Bcn) + Jellifish skies(Girona)
+12	07/04/2004	Twelve(Bcn)
+13	23/04/2004	Maple(Bcn)
+14	05/06/2004	Hopeful(Sfg) + Based on a lie(Torello) + Realising hate
+15	20/06/2004	Trasto(Bcn) + Dj Jaume(Caldes de Malavella)
+16	26/06/2004	Callahans(Palafrugell) + Two Stones(Bcn) + Thee Suckin dicks(Madrid) + Holliwood sinners(Toledo)
+17	05/09/2004	Dr. Explosion(Gijon)+ Rock -a-Hulas(Galicia)+ The Capaces(Bcn)
+18	14/09/2004	Facedown in shit (Usa) + Outbreak(Vidreres) + Bikini Summer(Girona)
+19	04/12/2004	Supersucker(Usa) + Callahans(Palafrugell)
+20	13/06/2005	Borderline'66 dj (Vidreres)
+21	18/06/2005	Texas Terri (Usa) + The Masons(Usa) + Copache(Tossa de Mar)
+22	06/07/2005	Syncopated blue dj(Vidreres)
+23	10/07/2005	Kids Frncoise(Uk) + El Chico con la espina en el costado(Girona)
+24	15/07/2005	Callahans(Palafrugell) + Frednoise dj (Vidreres)
+25	20/07/2005	Voice of people dj(Vidreres)
+26	27/07/2005	Frednoise dj(Vidrers()
+27	05/09/2005	Hoods(Usa) + Awaken (Fr) + Death knell of gaia(Bcn)
+28	13/10/2005	The Movement (Dinamarca)
+29	29/10/2005	Pale Man made (Uk)
+30	11/11/2005	(lo:mueso)(Maresme) + Biscuit(Vilanova) + No more lies(Sfg)
+31	12/11/2005	Josep Tero (L'Escala)
+32	05/01/2006	Illinoise(Vidreres) + Human ashtray(Vidreres) + Criatures fastigoses(Vidreres) + dj Pauk(Caldes de Malavella)
+33	27/01/2006	Jalea Real + Missing Dog Head
+34	06/03/2006	The Briefs (Usa)
+35	30/04/2006	Muletrain(Madrid) + Clarice(Sfg)
+36	17/06/2006	Lula(Valencia) + Las Perras del infierno(Madrid) + Thee Girlfriends(Madrid) dj blue moon + las perras djs
+37	31/08/2006	Two Dead Cats(Girona) + illinoise(Vidreres) + Rebel Girl dj(Vidreres)
+38	25/09/2006	Valina( Austria)+ frednoise dj
+39	08/10/2006	September Malevolence (Suecia) + Meltselectors djs
+40	23/10/2006	The Montesas (alemania) + Black Knights (Alemania)
+41	22/11/2006	Dick Dale(Usa) + Los Tiki Phantoms(Bcn)
+42	24/11/2006	The Saints (Australia) + Supercrapules(Vidreres)
+43	05/03/2007	Supersuckers(Usa)
+44	08/03/2007	Bob Log III (Usa) + Frednoise dj
+45	10/03/2007	Four crazy horses(Macanet de la Selva)
+46	13/04/2007	Souvaris(Uk) + Gareth Hardwick(Uk) + Meltselectors djs
+47	14/04/2007	The Chealsea Smiles(Usa)
+48	07/05/2007	The Morlocks(Usa) + Tito and the Brainsuckers(Italia)
+49	19/05/2007	Monochrome(Alemania) + No moe lies(Sfg) + Half foot outside(Pamplona) + illinoise(Vidreres) + rebel dj
+50	09/06/2007	The Kids(Belgica) + Hollywood sinners(Toledo) + Tokio sex destruction(Bcn)+ paul portobello dj + Blow up dj
+51	15/06/2007	Ultra Dolphins( Ussa)+We can't be lovers(Sfg)
+52	21/07/2007	Beast of burbon(Australia) + La Banda del Yuyu(Girona)
+53	30/08/2007	Triangulo de Amor Bizarro(Galicia) + Bullitt(Sfg)+ New Day rising( + Atzavara girls djs
+54	09/11/2007	Haram (Usa) + The Mistake(Canaries)
+55	01/12/2007	The Warlocks(Usa) + Memorikol(Vidreres)
+56	13/03/2008	Bloodlights (Noruega) + Electric Wasted(Bcn)
+57	17/05/2008	Subsonics(Usa) + Tiger Tiger (Usa) + Almandino quite deluxe(Italia) + The Educators(Palafrugell)
+58	22/05/2008	The Black Halos (Canada) + Travis Bickle(Bcn)
+59	05/06/2008	Super 400 (Usa)
+60	07/06/2008	Spraydog(Escocia) + Nueva Vulcano(Bcn) + Two dead Cats(Girona) +illinoise(Vidreres)
+61	03/09/2008	Lazy Juo (Sfg)
+62	04/09/2008	Kitsch(Banyoles) + Madee(Premia)
+63	14/11/2008	Subterranean Kids(Bcn) + Willis Drummond (Fr) + Nan roig(Bcn)
+64	15/11/2008	Willis Drummond (Fr) + illinoise(Vidreres)
+65	10/12/2008	Fucked Up (Canada) + Fractured(Girona)
+66	05/01/2009	Holliwood Sinners(Toledo) + Dj blow up
+67	31/01/2009	(lo:mueso) (Maresme) + frednoise dj
+68	14/02/2009	Bullitt(Sfg) + Dj Waiem
+69	13/03/2009	Do:Rudiguer (Pais Basc)
+70	27/03/2009	Los Tiki Phantoms(Bcn)
+71	28/03/2009	Songstore(Bcn)
+72	18/04/2009	Subterranean Kids(Bcn) + Fills de picciotto djs
+73	16/05/2009	Jim Jones Revue(Uk) + The deadly vibes(Usa) + Old Wilson(Bcn) + Blue Moon dj
+74	27/05/2009	Sisterhood Issue (Fr) + Rebel Girl dj
+75	06/06/2009	Half Foot outside(Pamplona) + lo mueso(Maresme) + Capsula(Pais Basc) + Hot Gossip(italia) + Hurricade(Girona)
+76	12/07/2009	Soulfly(Brasil) + Human Ashtray
+77	03/09/2009	Memorikol(Vidreres) + Las Victimas(Girona)
+78	05/09/2009	SA(Pais Basc)
+79	09/10/2009	TV Buddhas(Israel)
+80	13/11/2009	Marvins revolt(Dinamarca)+ DjPeixito
+81	14/11/2009	The Lizards(Bcn) + Dj Waiem
+82	27/11/2009	Nueva Vulcano(Bcn) + Vistalegre(Sfg)
+83	28/11/2009	Cortona(Fr) + MGZ dj
+84	07/12/2009	Carusella(Israel) + Rebel girl dj
+85	31/12/2009	Soroll djs
+86	13/02/2010	Chuck Violence(Brasil) + Thee Teenage monlight borderliners (Fr) + Monkeybrain'66 dj + Blue Moon dj
+87	20/02/2010	The Mistake(Canaries) + Meltselectors djs
+88	25/02/2010	Willis Drummond(Fr) + Deline(Bcn)
+89	25/02/2010	Willis Drummond (Fr)
+90	27/02/2010	Triangulo de Amor Bizarro(Galicia) + (lo:mueso)(Maresme)
+91	09/04/2010	Monegre(Girona) + Cohira(Girona)
+92	01/05/2010	Flying Over (Fr)
+93	10/06/2010	I Actitud Fest 12 grups
+94	08/10/2010	Mostros(Mallorca) + The Educators(Palafrugell)
+95	17/10/2010	Willis Drummond(Fr) + Hurricade(Girona) + illinoise(Vidreres)
+96	23/10/2010	Hurricade(Girona) + Fills de picciotto djs
+97	25/10/2010	Gareth Dickson( UK)
+98	12/11/2010	Memorikol(Vidreres) + La banda del nervio(Bcn)
+99	13/11/2010	Action Beat(Uk) + Frednoise dj
+100	20/11/2010	Truman Waters(Usa) + Aliment(Girona) + Dj Waiem
+101	08/01/2011	illinoise(Vidreres)+ dj sisset
+102	14/01/2011	Caroline and the Treats(Noruega)
+103	11/02/2011	Flying Over (Fr) + The Lizards(Bcn)
+104	19/02/2011	The Capaces(Bcn)
+105	26/02/2011	Go Chic(Taiwan)
+106	04/03/2011	illinoise(Vidreres)
+107	11/03/2011	Milenrama(Sarria de Ter) + Fi-asko(Sarria de Ter)
+108	19/03/2011	Golden Helmets (Alemania)
+109	03/06/2011	Cobra(Pais Basc) + illinoise(Vidreres)
+110	10/06/2011	II Actitud fest 15 grups
+111	11/06/2011	Hard-ons(Australia)
+112	23/06/2011	Adolescents (Usa) + Godfarts(Bcn)
+113	10/09/2011	Bastards Assault(Bcn)
+114	22/09/2011	FP(Vic) + Goliath( Valles Oriental)
+115	21/10/2011	The Afganistan Yeye's(La roca) + Extensity(Vic) + Fanxicore dj
+116	06/11/2011	Lac la Belle(Usa) + Ask the dust(sfg)
+117	12/11/2011	Joseba Irazoki(Pais Basc)
+118	02/12/2011	Suzy y los Quattro(Bcn)
+119	14/01/2012	The Brontes(Galicia) + Frednoise dj
+120	21/01/2012	Eric fuentes y el mal menor(Bcn)
+121	04/02/2012	Her only Presence(Bcn) + Dj Ecualizador
+122	18/02/2012	(lo:mueso)(Maresme)
+123	01/03/2012	Last Days of April(Suecia) + Bullitt(Sfg)
+124	25/03/2012	Leatherface (Uk) + Turnstile(Manlleu) + Tropical Iceland(torello)
+125	12/05/2012	Betunizer(Valencia) + Dj Brotx
+126	07/06/2012	III Actitud Fest 17 grups
+127	29/06/2012	Human Ashtray(Vidreres)+ Criatures Fastigoses(Vidreres)
+128	30/08/2012	Human Ashtray(Vidreres) + Klan Fukushima(Banyoles) + Trempant(Riudarenes)
+129	10/11/2012	Joan Colomo(Sant Celoni) + Eric Fuentes y el mal menor(Bcn) + Lazy juo(Sfg)
+130	16/11/2012	Hurricade(Girona) + Aliment(Girona) + The Lizards(Bcn) +Please Wait(Sfg)
+131	20/12/2012	Duncan redmnonds(Uk) + The missing Leech(Sant joan de Vilatorrada)
+132	22/12/2012	Burning Bus(Vidreres) + The dealers + Human Ashtray + El secreto de dorothy malone + illinoise
+133	03/04/2013	Willis Drummond(Fr) + Tropical Iceland(Torello)
+134	11/04/2013	Willis Drummnod(Fr)
+135	12/04/2013	Willis Drummond(Fr) + illinoise(Vidreres)
+136	06/06/2013	IV Actitud fest : 16 grups
+137	03/08/2013	Tempant(Riudarenes) + Benzine(Blanes) + illinoise(Vidreres)
+138	29/08/2013	Please Wait(Sfg) + Craitures Fastigoses(Vidreres) + I'M(Manlleu)
+139	11/10/2013	Bitch Queens( Suissa) + Impudics(Palafrugell)
+140	20/12/2013	Festa aniversari 10 anys: 10 grups
+141	18/01/2014	Sed non satiata(Fr) + Hurricade+ illinoise + the luju dj
+142	24/01/2014	The Holybuttons(Bcn) + Impudics
+143	29/01/2014	Willis Drummond(fr) + frednoise Dj
+144	22/02/2014	Esperit(sant Celoni) + Ask the dust(Sfg)
+145	22/03/2014	Go Popitas(Hospitalet) + The Crazies(Palafrugell)
+146	12/04/2014	Savage Riposte(Fr) + storm dj
+147	26/04/2014	Romanes(Bcn) + El Secreto de dorothy malone(Vidreres)
+148	07/06/2014	Crim(Tarragona) + Rot(Emporda)
+149	12/07/2014	Hidden Cabins(Usa) + Siren and Shelter(Uk)
+150	15/07/2014	V Actitud fest: 23 grups
+151	03/10/2014	Feed me more(italia) + Himura(saragossa) + Criatures Fastigoses(Vidreres)
+152	17/10/2014	Rvivr(Usa) + Anchord(Sarria de ter)
+153	28/11/2014	TCN(Sant Feliu de Codines) + FP(Vic)
+154	12/12/2014	Autodestruccio(Hostalric) + Rot(Emporda)
+155	18/12/2014	Viva Belgrado(Cordoba)
+156	20/12/2014	Rouille(saragossa) +Enze(Torello)
+157	21/02/2015	La Tosca Brava(Girona) + Frednoise dj
+158	29/03/2015	Trono de sangre(Madrid) + Rebuig(Bcn)
+159	10/04/2015	Betunize(Valencia) + Frednoise dj
+160	11/04/2015	Navratilova(Sfg)
+161	12/04/2015	FOD (Belgica) + How to fuck it(Angles)
+162	24/04/2015	Territori under: Hurricade + Anchord + Navratilova + Trave cigar box
+163	09/05/2015	No more lies + Hurricade +Descubriendo a Mr. Mime + Frednoise dj
+164	15/05/2015	The Gamits(usa) + Dwayne(usa)
+165	27/05/2015	No//Se (Usa) + Kathon(Cassa de la Selva)
+166	29/05/2015	Human Ashtray(Vidreres) + Heresic Synopsis(Fr)
+167	31/07/2015	VI Actitud Fest : 21 grups
+168	24/09/2015	Peter Black (Austalia) + Forest Pooky (Fr)
+169	10/10/2015	Rvivr(Usa) + Molly G(Torrelles de Llobregat)
+170	13/11/2015	(lo:mueso)(Maresme) + Cala Vento(Emporda) + frednoise dj
+171	04/12/2015	Shut up twist again(Fr) + Bruts(Vidreres)
+172	05/12/2015	Mike noegraf(Fr) + Joan Colomo(Sant Celoni) + Navratilova(Sfg)
+173	06/12/2015	Joana Serrat(Vic) + The Lake Poets(Uk)
+174	27/12/2015	The Smudjas (Italia) + Tano! (Girona)
+175	28/03/2016	The Capaces + Disaster jacks
+176	09/04/2016	Za(Bcn) + Ekiza(Pais basc)+Cala vento(Emporda) + Matagalls(Montseny)
+177	17/04/2016	Mike noegraf(Fr)
+178	20/05/2016	Wild animals(Madrid) + Sictor Valdana and check this outs(Bcn)
+179	17/06/2016	Not Scientists(Fr) + Bullitt(Sfg)
+180	29/07/2016	VII Actitud Fest: 22 grups
+181	09/09/2016	Bitpart(Fr) + Crows an Wra (Uk)
+182	08/10/2016	Western Settings(Usa) + Fobia(Manlleu)
+183	17/10/2016	Viva Belgrado(Cordoba) + Nucli(Aiguafreda) + Dra. Golsfoot dj!
+184	30/10/2016	Not On tour(israel) + Duncan Redmonds(Uk) + How to fuck it(Angles)
+185	12/11/2016	Galgo(Elche) + Mendra(Sfg)
+186	25/11/2016	Senor No(Pais Basc) + Drinkin brothers djs(Girona)
+187	08/12/2016	Paul Fuster(Usa-cat) + Xavier Calvet(Sfg)
+188	09/12/2016	Calido home(Bcn) + Les cruet(Sant Celoni) + Hugo Race(Autralia)
+189	10/12/2016	Sr. Chinarro(Sevilla) + Cala vento(Emporda)
+190	27/01/2017	Human ashtray + Billi idiot +
+191	24/02/2017	Tokio sex destruction(Vilanova) + Bad mongos(Palafrugell)
+192	07/04/2017	Rollercoaster kills(Madrid) + Cruise(Girona)
+193	26/05/2017	Clorox Boy dj + Kim jong punk dj + Unclu garrot dj Bar la Pedra-Girona
+194	27/05/2017	Caves(Uk) + Bullitt(SFG) + Dj Batrans
+195	28-30/7/17	Actitud Fest 2017
+196	21/10/2017	Jamie 4 President(Malaga) + Llacuna(Girona) + Topsy turvy's(Franca)
+197	11/11/2017	Tim Vantol(Holanda) + Tim Van Doorn(Belgica) IV Folkyou
+198	12/11/2017	Matthew Mcdaid(Belfast) IV Folkyou 2017
+199	02/12/2017	Lawine(Holanda) + Panellet (Terrassa) + Dj Morlac
+200	26/01/2018	Ekiza(Baiona) Al Casal el Forn de Girona
+201	28/01/2018	Ekiza(Baiona) Bar la Piscina
+202	16/02/2018	Tunic(Canada) + Mas Squad(Bcn)
+203	29/04/2018	Xavier Calvet (Sant Feliu de guixols) Bar la Piscina
+204	01/05/2018	Heavy Heart(Franca) + Morgan Cash(Llanca) Bar la piscina
+205	18/05/2018	Aina(Bcn) + Vistalegre(SFG) + Les Cruet (Montseny) Bar la Piscina
+206	25/05/2018	Cuello (Valencia) + Aldrin y Collins(Bcn) Bar la Piscina
+207	27/07/2018	Actitud Fest 2018
+208	08/12/2018	Not Scientists(Fr) + Serpent(Bcn)
+209	01/02/2019	Anteros(Bcn) + Runa(Bcn)
+210	16/02/2019	illinoise(Vidreres) + Ansia(Hospitalet) + Bonestorm(Vic)
+211	04/05/2019	Dave Smalley&Bandoleros(Usa) + Sictor Valdana(Bcn)
+212	18/07/2019	Actitud Fest 2019
+213	10/11/2019	Alba(EH)
+214	23/11/2019	Salina(Bcn) + Secondbest(Bcn) + Bomba de humo djs
+215	01/12/2019	Eric Fuentes (Bcn)
+216	24/01/2020	The Blackjaw(Madrid) + Llacuna (Girona)
+217	07/02/2020	The Overjoyed(Grecia) + Fetus (La Bisbal d'Emporda)
+218	15/02/2020	Kugo (Madrid) + Copdemar (Girona)
+219	08/03/2020	Xavier Calvet (Sant Feliu de guixols) + banda Bar la Piscina`;
+
+const archiveEntries = archiveData.split('\n').map((line) => {
+  const [number, date, ...rest] = line.split('\t');
+  return {
+    number,
+    date,
+    bands: rest.join('\t'),
+  };
+});
+
+const groupedArchive = archiveEntries.reduce((acc, entry) => {
+  const parts = entry.date.split('/');
+  const rawYear = parts[parts.length - 1];
+  const year = rawYear.length === 2 ? `20${rawYear}` : rawYear;
+
+  if (!acc[year]) {
+    acc[year] = [];
+  }
+
+  acc[year].push(entry);
+  return acc;
+}, {});
+
+const archiveYears = Object.keys(groupedArchive).sort();
+
 function About() {
   return (
-    <section className="mx-auto max-w-6xl px-6 py-20">
-      <div className="rounded-3xl border border-white/10 bg-white/5 p-8">
-        <h1 className="text-3xl font-semibold">About</h1>
-      </div>
-    </section>
-  )
+    <div className='bg-black text-white'>
+      <section className='bg-[#f4f1ec] px-6 py-16 text-black sm:px-10 lg:px-16'>
+        <div className='mx-auto w-full max-w-[1200px]'>
+          <div className='grid gap-10 lg:grid-cols-[minmax(0,0.9fr)_minmax(0,1.1fr)]'>
+            <div className='max-w-sm'>
+              <h2 className=' text-4xl font-black uppercase tracking-[-0.06em] sm:text-5xl'>
+                L'associació Soroll
+              </h2>
+            </div>
+
+            <div className='max-w-3xl space-y-5 text-sm leading-relaxed text-black/72 sm:text-[15px]'>
+              <p>
+                Després de gaudir i admirar com a públic durant molts anys la
+                filosofia i tarannà de l’Atzavara Club de Sant Feliu de Guíxols,
+                l’any 2002 ens decidim fer una cosa semblant al nostre poble:
+                Vidreres. Així és com comencem a organitzar bolos sota la
+                filosofia del “Do It Yourself” amb el nom de Freakorange.
+              </p>
+
+              <p>
+                L’any 2003 Freekorange desapareix i es crea el col·lectiu
+                anomenat Vidcore, que posteriorment es divideix amistosament en
+                dues associacions i així és com neix l’associació Soroll.
+              </p>
+
+              <p>
+                Des de la nostra fundació, un dels objectius ha estat aconseguir
+                un espai fix on poder organitzar els nostres concerts però a dia
+                d’avui encara seguim lluitant!!
+              </p>
+
+              <p>
+                Sota el nom de Soroll i Vidcore, a part de les diferents
+                edicions de l’Actitud Fest, s’han organitzat fins a l’actualitat
+                més de 200 concerts en els quals han participat més de 500
+                bandes, tot això gràcies a la dedicació desinteressada de
+                membres i col·laboradors de Soroll.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='bg-[#f4f1ec] px-6 py-16 text-black sm:px-10 lg:px-16'>
+        <div className='mx-auto w-full max-w-[1200px]'>
+          <div className='grid gap-10 lg:grid-cols-[minmax(0,0.9fr)_minmax(0,1.1fr)]'>
+            <div className='max-w-sm'>
+              <h2 className='text-4xl font-black uppercase tracking-[-0.06em] sm:text-5xl'>
+                Arxiu de concerts
+              </h2>
+            </div>
+
+            <div className='space-y-8'>
+              {archiveYears.map((year) => (
+                <section
+                  key={year}
+                  className='border-t border-black/10 pt-5 first:border-t-0 first:pt-0'
+                >
+                  <div className='sticky top-16 z-10 mb-4 w-fit rounded-full border border-black/10 bg-[#f4f1ec]/95 px-3 py-1 backdrop-blur'>
+                    <p className='text-[11px] font-black uppercase tracking-[0.2em] text-black/55'>
+                      {year}
+                    </p>
+                  </div>
+
+                  <div className='space-y-2'>
+                    {groupedArchive[year].map((entry) => (
+                      <article
+                        key={entry.number}
+                        className='grid gap-2 rounded-lg border border-black/8 bg-black/[0.02] px-4 py-3 sm:grid-cols-[72px_110px_minmax(0,1fr)] sm:items-start sm:gap-4'
+                      >
+                        <p className='text-[11px] font-black uppercase tracking-[0.18em] text-black/45'>
+                          #{entry.number}
+                        </p>
+                        <p className='text-[11px] font-black uppercase tracking-[0.16em] text-black/52'>
+                          {entry.date}
+                        </p>
+                        <p className='text-sm leading-relaxed text-black/72'>
+                          {entry.bands}
+                        </p>
+                      </article>
+                    ))}
+                  </div>
+                </section>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default About
+export default About;
